c34ea5f455c2e914c92c596b4c1799ba98e7d758
Andrew Bui <andrew.bui.au@gmail.com>
2020-11-03 19:11:45 +1100

    added forecast functionality to application

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -7,21 +7,35 @@ class CitiesController < ApplicationController
         require 'net/http'
         require 'json'
 
-        @city = params[:name.to_s]&.downcase
+        @city = params[:name.to_s]
+
+        # Current Weather
         @url = "https://api.openweathermap.org/data/2.5/weather?q=#{@city}&appid=b3de74108cc43d86835f3312a5074990"
         @uri = URI(@url)
         @response = Net::HTTP.get(@uri)
         @output = JSON.parse(@response)
 
+        # Current Weather Outputs
         @temp = @output[:main.to_s][:temp.to_s]
+
+        # Forecast Weather
+        @forecast_url = "https://api.openweathermap.org/data/2.5/forecast?q=#{@city}&appid=b3de74108cc43d86835f3312a5074990"
+        @forecast_uri = URI(@forecast_url)
+        @forecast_response = Net::HTTP.get(@forecast_uri)
+        @forecast_output = JSON.parse(@forecast_response)
+
+        # Forecast Weather Outputs
+        @total = @forecast_output["list"]
 
         add
 
         @search = Search.order(id: :desc).limit(3)
     end
 
+
+
     def add
-        Search.create(search: params[:name.to_s]&.downcase)
+        Search.create(search: params[:name.to_s])
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_223915) do
+ActiveRecord::Schema.define(version: 2020_11_03_023149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "weathers", force: :cascade do |t|
-    t.string "city"
+  create_table "searches", force: :cascade do |t|
+    t.string "search"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
